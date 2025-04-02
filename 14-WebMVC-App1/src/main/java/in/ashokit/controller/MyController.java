d5894b7f59c1fd501d49984224daaee99a3e78a0
KANHU CHARAN BEHERA <beherakanha029@gmail.com>
2025-04-02 15:42:26 +0530

    Add files via upload

---
+++ 14-WebMVC-App1/src/main/java/in/ashokit/controller/MyController.java
@@ -0,0 +1,30 @@
+package in.ashokit.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class MyController {
+
+	@GetMapping("/welcome")
+	public ModelAndView getWelComeMsg(@RequestParam String name) {
+		String  msgTxt =name +", Well come to Ashok IT ..";
+		ModelAndView mav=new ModelAndView();
+		mav.addObject("msg", msgTxt);
+		mav.setViewName("index");
+		
+		return mav;
+		
+	}
+	
+	@GetMapping("/greet")
+	public String getGreetMsg(@RequestParam String name,Model model) {
+		model.addAttribute("msg", name +",Good Evening....!!");
+		return "index";
+		
+	}
+	
+}
---
+++ 14-WebMVC-App1/src/main/java/in/ashokit/controller/WelcomeController.java
@@ -0,0 +1,29 @@
+package in.ashokit.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+
+public class WelcomeController {
+	
+	@GetMapping("/welcome")
+	public ModelAndView getWelcomeMsg() {
+		ModelAndView mav=new ModelAndView();
+		mav.addObject("msg","Welcome To Ashok IT");
+		mav.setViewName("index");
+		return mav;
+		
+	}
+	
+	
+	@GetMapping("/greet")
+	public ModelAndView getGreetMsg() {
+		ModelAndView mav=new ModelAndView();
+		mav.addObject("msg","Good Evening... !!");
+		mav.setViewName("index");
+		return mav;
+		
+	}
+}
